bd10fdd39769e200c51b67b5372bdf62c2bf3ab4
sarahcocknor <38953103+yalunar1@users.noreply.github.com>
2019-11-20 11:12:44 +0300

    Log fix

--- ControllerEmulator/Console.cs
+++ ControllerEmulator/Console.cs
@@ -10,28 +10,18 @@ namespace ControllerEmulator
 
         static readonly string _logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.log");
 
-        public static void WriteLine()
-        {
-            File.AppendAllText(_logPath, Environment.NewLine);
-            System.Console.WriteLine();
-        }
-
         public static void WriteLine(string line)
         {
+            try
+            { 
             File.AppendAllText(_logPath, line + Environment.NewLine);
             System.Console.WriteLine(line);
+            }
+            catch (Exception e)
+            {
+            System.Console.WriteLine(line);
+            }
         }
 
-        public static void Write(string line)
-        {
-            File.AppendAllText(_logPath, line);
-            System.Console.Write(line);
-        }
-
-        public static void Write(int n)
-        {
-            File.AppendAllText(_logPath, n.ToString());
-            System.Console.Write(n);
-        }
     }
 }
