baa64d156d058c5f96e8818086863e928a78c367
yalunar <yalunar1@gmail.com>
2019-11-20 11:17:55 +0300

    log fix

--- ControllerEmulator/Console.cs
+++ ControllerEmulator/Console.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
@@ -26,32 +25,3 @@ namespace ControllerEmulator
 
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-
-namespace ControllerEmulator
-{
-    static class Console
-    {
-
-        static readonly string _logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.log");
-
-        public static void WriteLine(string line)
-        {
-            try
-            { 
-            File.AppendAllText(_logPath, line + Environment.NewLine);
-            System.Console.WriteLine(line);
-            }
-            catch (Exception e)
-            {
-            System.Console.WriteLine(line);
-            }
-        }
-
-    }
-}
->>>>>>> bd10fdd39769e200c51b67b5372bdf62c2bf3ab4
